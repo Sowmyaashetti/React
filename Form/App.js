c143b38b837db99a42c918e9f8e07043d469a089
Sowmyaashetti <111297983+Sowmyaashetti@users.noreply.github.com>
2023-07-22 19:33:12 +0530

    Add files via upload

---
+++ Form/App.js
@@ -0,0 +1,60 @@
+import "./App.css";
+import { Form } from "./Components/Form";
+
+function App() {
+  return (
+  <div className="App">
+    <Form />
+     </div>
+  );
+}
+
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+
+
+
+
+// function App() {
+  
+//   return <div className="App"> </div>;
+// }
+
+
+
+// export default App;
